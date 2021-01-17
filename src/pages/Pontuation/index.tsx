9762cf9e4b6ddbfd717eb0ae2150a7b0436c2da9
rafaelfachinelli <rafael.fachinelli@fatec.sp.gov.br>
2021-01-17 15:40:47 -0300

    feat: my pontuation page

--- src/pages/Pontuation/index.tsx
+++ src/pages/Pontuation/index.tsx
@@ -7,6 +7,13 @@ import Notify from '../../components/Notifybar';
 
 import dashboardImage from '../../assets/images/dashboard.png';
 import avatarImage from '../../assets/images/avatar.png';
+import activityPorcentageImage from '../../assets/images/activity-porcentage.png';
+
+import grafic1Image from '../../assets/images/grafic1.png';
+import grafic2Image from '../../assets/images/grafic2.png';
+
+
+import rankingIco from '../../assets/icons/ranking.svg';
 
 const Pontuation: React.FC = () => {
   return (
@@ -14,6 +21,80 @@ const Pontuation: React.FC = () => {
       <Sidebar />
       <Content>
         <Notify />
+        <p className="pontuation-welcome">Minha Pontuação</p>
+        <div className="pontuation-stats">
+          <div className="stats-XP">1.000 <br/>XP</div>
+          <div className="stats-ranking">
+            <p>Sua colocação no Ranking:</p>
+            <div className="stats-position">
+              <img src={rankingIco} alt="Rank Hoje"/>
+              <p>2º colocado</p>
+              <p>hoje</p>
+            </div>
+            <div className="stats-position">
+              <img src={rankingIco} alt="Rank Mensal"/>
+              <p>3º colocado</p>
+              <p>mensal</p>
+            </div>
+            <div className="stats-position">
+              <img src={rankingIco} alt="Rank Geral"/>
+              <p>34º colocado</p>
+              <p>geral</p>
+            </div>
+          </div>
+          <div className="pontuation-points">
+            <p className="points-intro">Minha pontuação:</p>
+            <span className="points">24</span>
+            <p className="legend">pontos</p>
+            <button>Resgatar prêmio</button>
+          </div>
+        </div>
+
+        <div className="statistics-user">
+
+        </div>
+
+        <div className="informations-statistics">
+          <div>
+            <span className="statistics-title">Estatísticas de acertos</span>
+            <div className="statistics-background">
+
+              <div className="statistic-ativity">
+                <p className="statistic-name">Marketing</p>
+                <img src={activityPorcentageImage} alt="Activity Porcentage"/>
+              </div>
+
+              <div className="statistic-ativity">
+                <p className="statistic-name">Psicologia</p>
+                <img src={activityPorcentageImage} alt="Activity Porcentage"/>
+              </div>
+
+              <div className="statistic-ativity">
+                <p className="statistic-name">Matemática</p>
+                <img src={activityPorcentageImage} alt="Activity Porcentage"/>
+              </div>
+
+              <div className="statistic-ativity">
+                <p className="statistic-name">Programação</p>
+                <img src={activityPorcentageImage} alt="Activity Porcentage"/>
+              </div>
+
+              <div className="statistic-ativity">
+                <p className="statistic-name">Excel</p>
+                <img src={activityPorcentageImage} alt="Activity Porcentage"/>
+              </div>
+
+            </div>
+          </div>
+          
+
+          <div className="statistic-grafic">
+            <p className="grafic-title">Desempenho</p>
+            <img className="grafic-1" src={grafic2Image} alt="Gráfico 1"/>
+            <img className="grafic-2" src={grafic1Image} alt="Gráfico 2"/>
+          </div> 
+        </div>
+
       </Content>
     </Container>
   );
--- src/pages/Pontuation/styles.ts
+++ src/pages/Pontuation/styles.ts
@@ -14,286 +14,139 @@ export const Content = styled.div`
   align-items: center;
   padding: 1rem;
 
-  .content-welcome {
+  .pontuation-welcome {
     width: 100%;
     max-width: 940px;
+    margin-bottom: 1rem;
 
     color: var(--color-text-dark);
     font-size: 3.6rem;
     font-weight: 700;
   }
 
-  .content-header {
+  .pontuation-stats {
+    height: 320px;
     width: 100%;
     max-width: 940px;
     display: flex;
     flex-direction: row;
-    justify-content: space-between;
-    align-items: flex-end;
-  }
-  
-  .content-news {
-    max-height: 23rem;
-    max-width: 59rem;
-    padding: 1rem;
+    justify-content: space-around;
+    align-items: center;
+    margin-bottom: 5rem;
 
     background: var(--color-secondary);
     border-radius: 2rem;
   }
 
-  .content-news img {
-    height: 100%;
-    width: 100%;
-  }
-
-  .myPoints-title {
-    margin-bottom: 1.5rem;
-
-    color: var(--color-text-dark);
-    font-size: 2rem;
-  }
-
-  .myPoints-background {
-    height: 20rem;
-    width: 18.5rem;
+  .stats-XP {
+    height: 214px;
+    width: 214px;
     display: flex;
-    align-items: center;
     justify-content: center;
-
-    background: var(--color-secondary);
-    border-radius: 2rem;
-  }
-
-  .myPoints-points {
-    height: 130px;
-    width: 130px;
-    display: flex;
     align-items: center;
-    justify-content: center;
 
     background: var(--color-secondary-light);
     color: var(--color-text-light);
-    font-size: 3rem;
-    text-align: center;
     border-radius: 50%;
+    font-size: 4rem;
+    text-align: center;
   }
 
-  .content-informations {
+  .stats-ranking {
+    color: var(--color-text-light);
+    font-size: 1.6rem;
+  }
+
+  .stats-position {
+    display: flex;
+    justify-content: space-between;
+    margin: 1rem 0;
+  }
+
+  .pontuation-points {
+    width: 265px;
+    height: 214px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    background: var(--color-secondary-light);
+    color: var(--color-text-light);
+    border-radius: 2rem;
+  }
+
+  .points-intro {
+    margin-bottom: 1.5rem;
+
+    font-size: 1.8rem;
+  }
+
+  .points {
+    font-size: 6rem;
+  }
+
+  .pontuation-points button {
+    margin-top: 1rem;
+    padding: .5rem 1rem;
+
+    background: #6E3DC8;
+    color: var(--color-text-light);
+    border: none;
+    border-radius: 20px;
+  }
+
+  .informations-statistics {
     width: 100%;
     max-width: 940px;
     display: flex;
-    flex-wrap: wrap;
     justify-content: space-between;
-    margin-top: 1rem;
   }
 
-  .activities-title {
+  .statistics-title {
     color: var(--color-text-darker);
     font-size: 2rem;
   }
 
-  .activities-button {
-    width: 100px;
-    padding: .3rem;
-    margin-left: 1rem;
-
-    background: #C4C4C4;
-    color: #fff;
-    font-size: 1.4rem;
-    border-radius: 2rem;
-    border: none;
-  }
-
-  .activities-background {
-    max-width: 320px;
-    margin-top: 1rem;
-    padding: 1rem;
-
-    background: var(--color-background-informations);
-    border-radius: 2rem;
-  }
-
-  .activity {
-    width: 300px;
-    margin-bottom: 1.5rem;
-    padding: .7rem;
-    transition: .2s;
-
-    background: var(--color-background-informations);
-    border-radius: 1.5rem;
-  }
-
-  .activity:hover {
-    transform: scale(1.01);
-    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-  }
-
-  .activity-header {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-  }
-
-  .activity-name {
-    color: var(--color-text-darker);
-    font-size: 2rem;
-    font-weight: 700;
-  }
-
-  .activity-response {
-    padding: .5rem;
-    transition: .2s;
-
-    background: #fff;
-    color: #000;
-    font-size: 1.4rem;
-    border: none;
-    border-radius: 2rem;
-    cursor: pointer;
-  }
-
-  .activity-response:hover {
-    color: #fff;
-    background: var(--color-green);
-  }
-
-  .activity-progress {
-    height: 14px;
-    width: 100%;
-    margin-top: 1rem;
-    background: #C4C4C4;
-    border-radius: 2rem;
-  }
-  
-  .recomendations-title {
-    color: var(--color-text-darker);
-    font-size: 2rem;
-  }
-
-  .recomendations-background {
-    width: 230px;
-    margin-top: 1rem;
-    padding: 0.5rem;
-
-    background: var(--color-background-informations);
-    border-radius: 2rem;
-  }
-
-  .recomendation {
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    align-items: center;
-    margin-bottom: 1.5rem;
-    padding: .7rem;
-    transition: .2s;
-
-    background: var(--color-background-informations);
-    border-radius: 1.5rem;
-  }
-
-  .recomendation:hover {
-    transform: scale(1.01);
-    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-  }
-
-  .recomendation-name {
-    color: var(--color-text-darker);
-    font-size: 2rem;
-    font-weight: 600;
-  }
-
-  .points-number {
-    color: var(--color-green);
-    font-size: 2rem;
-    font-weight: 700;
-    text-align: center;
-  }
-
-  .points-legend {
-    color: var(--color-green);
-    font-size: 1.2rem;
-    font-weight: 700;
-  }
-
-  .recomendation-response {
-    padding: .5rem;
-    transition: .2s;
-
-    background: #fff;
-    color: #000;
-    font-size: 1.4rem;
-    border: none;
-    border-radius: 2rem;
-    cursor: pointer;
-  }
-
-  .recomendation-response:hover {
-    color: #fff;
-    background: var(--color-green);
-  }
-
-  .ranking-title {
-    color: var(--color-text-darker);
-    font-size: 2rem;
-  }
-
-  .ranking-background {
+  .statistics-background {
     width: 345px;
-    margin-top: 1rem;
-    padding: 0.5rem;
+    height: 355px;
+    padding: 1.5rem;
+    margin-top: 1.5rem;
 
-    background: #798BDE;
+    background: var(--color-background-informations);
     border-radius: 2rem;
   }
 
-  .ranking-types {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    align-items: center;
-    padding: 1rem;
-
-    border-bottom: 1px solid #fff
-  }
-
-  .ranking-types span {
-    color: var(--color-text-light);
-    font-size: 2rem;
-    cursor: pointer;
-  }
-
-  .student {
-    width: 100%;
+  .statistic-ativity {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
-    padding: .7rem;
   }
 
-  .student--gold img {
-    border-color: #f1c40f;
+  .statistic-ativity + .statistic-ativity {
+    margin-top: 2rem;
   }
 
-  .student-avatar {
-    height: 6rem;
-    width: 6rem;
-
-    border: .5rem solid #fff;
-    border-radius: 50%;
+  .statistic-name {
+    color: var(--color-text-darker);
+    font-size: 2.4rem;
   }
 
-  .student-name {
-    color: var(--color-text-light);
-    font-size: 2.5rem;
+  .statistic-grafic {
+    display: flex;
+    flex-direction: column;
   }
 
-  .student-XP {
-    color: var(--color-text-light);
+  .grafic-title {
+    margin-bottom: 2rem;
+
+    color: var(--color-text-darker);
     font-size: 2rem;
-    font-weight: 700;
+  }
+
+  .grafic-1 {
+    margin-bottom: 5rem;
   }
 `;
